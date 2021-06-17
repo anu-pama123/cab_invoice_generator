39fbda9099fdab92706dc568e6ca41874f33c992
anupama cv <anu.anupamacv@gmail.com>
2021-06-17 20:53:24 +0530

    Added: functionality to find total fare for the journey

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,5 +1,13 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
+    private static final  int COST_PER_TIME = 1;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
 
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < 5)
+            totalFare = 5;
+        return totalFare;
+    }
 }
--- src/test/java/com/bridhgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridhgelabz/InvoiceGeneratorTest.java
@@ -1,5 +1,16 @@
 package com.bridhgelabz;
 
-public class InvoiceGeneratorTest {
+import com.bridgelabz.InvoiceGenerator;
+import org.junit.Test;
+import org.junit.Assert;
 
+public class InvoiceGeneratorTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare,0.0);
+    }
 }
